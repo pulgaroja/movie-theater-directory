73798f27706909d095b2a444d58bbfcffc010e0e
Cristina Rojas <crojassoto@gmail.com>
2020-02-14 13:42:17 -0600

    Updates

---
+++ precache-manifest.52e06e3f5bd2f0e9c049fd7b499fc320.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcceb74eedffa36a02d954a39441f724",
+    "url": "/movie-theater-directory/index.html"
+  },
+  {
+    "revision": "62c04c9ea514674687f5",
+    "url": "/movie-theater-directory/static/css/main.81ce8fef.chunk.css"
+  },
+  {
+    "revision": "422e0ed6c4ba8e0f05fc",
+    "url": "/movie-theater-directory/static/js/2.743a5ce5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie-theater-directory/static/js/2.743a5ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c04c9ea514674687f5",
+    "url": "/movie-theater-directory/static/js/main.bd902851.chunk.js"
+  },
+  {
+    "revision": "b60b2671179f07d0bfca",
+    "url": "/movie-theater-directory/static/js/runtime-main.d36a303b.js"
+  },
+  {
+    "revision": "d26eab9e486b85bab34e2559b0bc2213",
+    "url": "/movie-theater-directory/static/media/headerBackground.d26eab9e.jpeg"
+  }
+]);
